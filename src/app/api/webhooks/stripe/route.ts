48954e6c6173f1467bfce65a9ad1c5098ed7c253
Renaanprofissional <renaanprofissional@gmail.com>
2025-11-16 06:38:38 -0300

    fix: correcting checkout

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -9,30 +9,30 @@ export const POST = async (request: Request) => {
   if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
     return NextResponse.error();
   }
+
   const signature = request.headers.get("stripe-signature");
-  if (!signature) {
-    return NextResponse.error();
-  }
+  if (!signature) return NextResponse.error();
+
   const text = await request.text();
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
   const event = stripe.webhooks.constructEvent(
     text,
     signature,
-    process.env.STRIPE_WEBHOOK_SECRET,
+    process.env.STRIPE_WEBHOOK_SECRET
   );
+
   if (event.type === "checkout.session.completed") {
-    console.log("Checkout session completed");
     const session = event.data.object as Stripe.Checkout.Session;
+
     const orderId = session.metadata?.orderId;
-    if (!orderId) {
-      return NextResponse.error();
-    }
+    if (!orderId) return NextResponse.error();
+
     await db
       .update(orderTable)
-      .set({
-        status: "paid",
-      })
+      .set({ status: "in_production" })
       .where(eq(orderTable.id, orderId));
   }
+
   return NextResponse.json({ received: true });
 };
