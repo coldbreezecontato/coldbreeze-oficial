ccf1b5a012ef270748e154181ec0c9828f37bc04
Renaanprofissional <renaanprofissional@gmail.com>
2025-11-20 08:15:10 -0300

    feat: add styles size-selector

--- src/app/product-variant/[slug]/components/size-selector.tsx
+++ src/app/product-variant/[slug]/components/size-selector.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { X } from "lucide-react";
 
 type SizeSelectorProps = {
   sizes: {
@@ -28,12 +29,32 @@ export default function SizeSelector({ sizes }: SizeSelectorProps) {
               key={s.id}
               onClick={() => !outOfStock && setSelected(s.id)}
               className={`
-                rounded-lg px-4 py-2 border text-sm transition-all
-                ${selected === s.id ? "bg-blue-300 border-black" : "bg-[#111827] border-white/10"}
-                ${outOfStock ? "opacity-40 cursor-not-allowed" : "cursor-pointer"}
+                relative flex items-center gap-2 rounded-lg px-4 py-2 border text-sm transition-all
+                ${selected === s.id
+                  ? "bg-blue-300 border-black text-black"
+                  : "bg-[#111827] border-white/10 text-white"
+                }
+                ${outOfStock
+                  ? "opacity-40 cursor-not-allowed"
+                  : "cursor-pointer hover:border-blue-400"
+                }
               `}
             >
-              {s.size.name}
+              {/* Nome do tamanho */}
+              <span>{s.size.name}</span>
+
+              {/* Estoque */}
+              <span className="text-xs opacity-80">
+                ({s.stock})
+              </span>
+
+              {/* X quando estiver sem estoque */}
+              {outOfStock && (
+                <X
+                  size={14}
+                  className="text-red-500 absolute w-full left-0 h-10"
+                />
+              )}
             </button>
           );
         })}
